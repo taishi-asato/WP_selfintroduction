f2e11dda4823570ae6e22a0a7806d84d4bcb2195
taishi-asato <taishi.asato@mixi.co.jp>
2020-01-15 09:22:24 +0900

    描画ツール作成

--- tools/main.js
+++ tools/main.js
@@ -4,8 +4,8 @@ window.onload = load;
 
 
 // 定数
-var SCREEN_WIDTH	= 40;
-var SCREEN_HEIGHT	= 20;
+var SCREEN_WIDTH	= 16;
+var SCREEN_HEIGHT	= 16;
 
 var MAIN_OBJ_RNO_0_INIT	= 0;
 var MAIN_OBJ_RNO_0_MAIN	= 1;
@@ -15,6 +15,56 @@ var MAIN_OBJ_RNO_0_MAIN	= 1;
 var gMainObj	= null;
 var gCharObjs	= null;
 var gScreenData	= [];
+
+
+/*=========================================
+	HTML上のドットを配列データに変換して出力する
+*=========================================*/
+function OnButtonClick(){
+//	■
+	console.log("押されたよ！")
+	
+	var DotsNum		= SCREEN_WIDTH * SCREEN_HEIGHT;
+	var ScreenDots = document.getElementById("ScreenDots");
+	var tempCounter	= 0;
+	var tempString	= "";
+	var TextBoxValue	= "";
+
+	var tempArray	= [];
+
+	// テキストボックスの中身が■になっているやつを覚える
+	for( var i=0; i<DotsNum; i++){
+		tempStrig	=  "ScreenDots.text" + i + ".value";
+		TextBoxValue	= eval(tempStrig);
+		if( TextBoxValue == "■"){
+			tempArray.push(i);
+		}
+	}
+
+	
+	// データ出力用の配列の準備
+	var OutputScreenData	= [];
+	for(var i=0; i<SCREEN_HEIGHT; i++){
+		OutputScreenData[i]	= [];
+		for(var j=0; j<SCREEN_WIDTH; j++){
+			OutputScreenData[i][j]	= 0;
+		}
+	}
+
+	// 出力する
+	var indexX = 0;
+	var indexY	= 0;
+	for( var i=0; i<tempArray.length; i++){
+		indexX	= tempArray[i] % SCREEN_WIDTH;
+		indexY	= Math.floor(tempArray[i] / SCREEN_WIDTH );
+		console.log("x..." +indexX + "  y..." +indexY );
+		OutputScreenData[indexY][indexX]	= 1;
+	}
+
+
+	console.log( OutputScreenData );
+}
+
 
 
 
@@ -76,8 +126,7 @@ function Loop() {
 *=========================================*/
 MainObject	= function(){
 
-	this.Hoge	= 123;
-	
+
 	this.Rno0	= 0;
 	this.Rno1	= 0;
 	this.Rno2	= 0;
@@ -112,10 +161,6 @@ MainObject.prototype.Rno0_Init = function(  ) {
 		}
 	}
 
-	gCharObjs	= new Array();
-	gCharObjs.push( new CharaObject("piyo", 40, 0, -0.7, 0) );
-	gCharObjs.push( new CharaObject("hoge", 60, 10, -0.7, -0.1) );
-
 	// 次の処理へ
 	this.ChangeRno0( MAIN_OBJ_RNO_0_MAIN );
 };
@@ -125,58 +170,9 @@ MainObject.prototype.Rno0_Init = function(  ) {
 *=========================================*/
 MainObject.prototype.Rno0_Main = function(  ) {
 
-	for(var i=0; i<gCharObjs.length; i++){
-		gCharObjs[i].pUpdateCharaPos();
-	}
-	this.DrawScreen();
-	
+	// 特に何もしない
+
 };
-
-/*=========================================
-スクリーン上にドットオブジェクトを描画
-*=========================================*/
-MainObject.prototype.DrawScreen = function(  ) {
-
-	// まず、スクリーンデータをクリアする
-	for(var i=0; i<gScreenData.length; i++){
-		for( var j=0; j<gScreenData[i].length; j++){
-			gScreenData[i][j]	= 0;
-		}
-	}
-
-	// スクリーンデータを管理している配列の更新
-	var CharaDotData	= null;
-	var ScreenPosX	= 0;
-	var ScreenPosY	= 0;
-	for( var i=0; i<gCharObjs.length; i++){
-		CharaDotData	= gCharObjs[i].Dotsdata;
-		for( var j=0; j<CharaDotData.length; j++){
-			for( var k=0; k<CharaDotData[j].length; k++){
-				ScreenPosX	= gCharObjs[i].PosX_Floor + k;
-				ScreenPosY	= gCharObjs[i].PosY_Floor + j;
-				if( ScreenPosX >=0 &&  ScreenPosX < SCREEN_WIDTH && ScreenPosY >=0 &&  ScreenPosY < SCREEN_HEIGHT){
-//					console.log("ScreenPosX..." +ScreenPosX + " j..." +j +"  k..." +k);
-					gScreenData[ScreenPosY][ScreenPosX]	= CharaDotData[j][k];
-				}
-			}
-		}
-	}
-
-	// スクリーンデータ配列を元に、HTMLに描画
-	var ScreenDots = document.getElementById("ScreenDots");
-	var tempCounter	= 0;
-	var tempString	= "";
-	for(var i=0; i<gScreenData.length; i++){
-		for( var j=0; j<gScreenData[i].length; j++){
-			tempStrig	=  "ScreenDots.text" + tempCounter + ".value	=''; "
-			if(gScreenData[i][j] == 1){
-				tempStrig	=  "ScreenDots.text" + tempCounter + ".value	='■'; "
-			}
-			eval(tempStrig);
-			tempCounter++;
-		}
-	}
-}
 
 
 /*=========================================
